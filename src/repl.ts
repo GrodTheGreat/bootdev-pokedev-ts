37a69554aa2965e8a714b5870fd4428bc1bdcd0d
Gerard Causse <grod.causse@gmail.com>
2026-04-28 22:29:11 -0400

    Complete repl lesson

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,23 @@
+import { createInterface } from "node:readline";
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "Pokedex > ",
+});
+
 export function cleanInput(input: string): string[] {
   return input.trim().toLowerCase().split(" ");
+}
+
+export function startREPL() {
+  rl.prompt();
+  rl.on("line", (line) => {
+    const args = cleanInput(line);
+    if (!args) {
+      rl.prompt();
+    }
+    console.log(`Your command was: ${args[0]}`);
+    rl.prompt();
+  });
 }
