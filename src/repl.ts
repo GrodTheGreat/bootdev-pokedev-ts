fdcae3fef3193592b62ae050aa77c8c0c12db978
Gerard Causse <grod.causse@gmail.com>
2026-04-28 22:43:34 -0400

    Complete help and exit lesson

--- src/repl.ts
+++ src/repl.ts
@@ -1,4 +1,7 @@
 import { createInterface } from "node:readline";
+import { commandExit } from "./command_exit.js";
+import type { CLICommand } from "./command.js";
+import { commandHelp } from "./command_help.js";
 
 const rl = createInterface({
   input: process.stdin,
@@ -11,13 +14,34 @@ export function cleanInput(input: string): string[] {
 }
 
 export function startREPL() {
+  const commands = getCommands();
   rl.prompt();
   rl.on("line", (line) => {
     const args = cleanInput(line);
     if (!args) {
       rl.prompt();
     }
-    console.log(`Your command was: ${args[0]}`);
+    const command = commands[args[0]];
+    if (command === undefined) {
+      console.log("Unknown command");
+      rl.prompt();
+    }
+    command.callback(commands);
     rl.prompt();
   });
+}
+
+export function getCommands(): Record<string, CLICommand> {
+  return {
+    help: {
+      name: "help",
+      description: "Displays a help message",
+      callback: commandHelp,
+    },
+    exit: {
+      name: "exit",
+      description: "Exits the pokedex",
+      callback: commandExit,
+    },
+  };
 }
